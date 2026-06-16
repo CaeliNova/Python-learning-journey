0e301f1053f36a082d57ab22b642fcd343713523
CaeliNova <gamesarcade51@gmail.com>
2026-06-17 01:44:02 +0800

    Add files via upload
    My first python program

---
+++ Helloworldtutorial.py
@@ -0,0 +1,6 @@
+
+
+firstNumber = int(input ("First Number: "))
+secondNumber = int(input ("Second Number: "))
+thirdNumber =firstNumber + secondNumber
+print (str(firstNumber) + " + " + str(secondNumber) + " = " + str(thirdNumber))
